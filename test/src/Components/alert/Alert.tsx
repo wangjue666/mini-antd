21607220f416cac43c9405f5a5e51a611223dc95
王珏 <wangjuesix@gmail.com>
2020-11-16 19:33:09 +0800

    弹窗出现 禁止背景滚动

--- test/src/Components/alert/Alert.tsx
+++ test/src/Components/alert/Alert.tsx
@@ -1,7 +1,11 @@
 import React from "react"
 import ReactDom from "react-dom"
 import Modal from "antd-mobile/lib/modal/index"
+import {Action} from "antd-mobile/lib/modal/PropsType"
 
+const stopMove = (e:any)=>{
+    e.preventDefault()
+}
 
 export default function alert(
     title: React.ReactNode,
@@ -10,9 +14,30 @@ export default function alert(
 ){  
     const div = document.createElement("div")
     document.body.appendChild(div)
-    const footer = actions.map(button=>{
 
+    const close = ()=>{
+        ReactDom.unmountComponentAtNode(div)
+        div.remove()
+        document.removeEventListener('touchmove', stopMove)
+    }
+
+    const footer = actions.map((button: Action<React.CSSProperties>)=>{
+        const oldOnPress = button.onPress || (()=>{})
+        button.onPress = ()=>{
+            const res = oldOnPress()
+            if(res && res.then){
+                res.then(close).catch(()=>{})
+            }else{
+                close()
+            }
+            
+        }
+
+        return button
     })
+
+    document.body.addEventListener('touchmove', stopMove, {passive: false})
+
     ReactDom.render(
         <Modal visible={true} transparent={true} title={title} closable={false} maskClosable={false} footer={footer} >
             <div>{message}</div>
