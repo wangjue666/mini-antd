29776407c5dc61e8b6e704f129ea88271bd5495e
王珏 <wangjuesix@gmail.com>
2020-11-19 18:39:13 +0800

    给menu组件添加测试用例

--- test/src/App.tsx
+++ test/src/App.tsx
@@ -21,6 +21,18 @@ function App() {
           Link 2
         </MenuItem>
       </Menu>
+
+      <Menu mode="vertical" onSelect={(index)=>console.log(index)} defaultIndex={0}>
+        <MenuItem index={0}>
+          Link 0
+        </MenuItem>
+        <MenuItem disabled index={1}>
+          Link 1
+        </MenuItem>
+        <MenuItem index={2}>
+          Link 2
+        </MenuItem>
+      </Menu>
     </div>
   );
 }
