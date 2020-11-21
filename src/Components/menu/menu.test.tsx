a44c506f0d3e72238b18f414ab257e0532a037aa
王珏 <wangjuesix@gmail.com>
2020-11-21 17:52:20 +0800

    给submenu组件添加单元测试

--- src/Components/menu/menu.test.tsx
+++ src/Components/menu/menu.test.tsx
@@ -1,15 +1,16 @@
 import React from "react"
-import { render, RenderResult, fireEvent, cleanup } from "@testing-library/react"
+import { render, RenderResult, fireEvent, cleanup, waitFor } from "@testing-library/react"
 import Menu, { MenuProps } from "./Menu"
 import MenuItem from "./MenuItem"
+import SubMenu from "./SubMenu"
 
 const testProps: MenuProps = {
-    defaultIndex: 0,
+    defaultIndex: '0',
     onSelect: jest.fn(),
     className: 'test',
 }
 const testVerProps: MenuProps = {
-    defaultIndex: 0,
+    defaultIndex: '0',
     mode: 'vertical'
 }
 const NiceMenu = (props:MenuProps) => {
@@ -17,16 +18,34 @@ const NiceMenu = (props:MenuProps) => {
         <Menu {...props}>
             <MenuItem> active </MenuItem>
             <MenuItem  disabled > disabled </MenuItem>
-            <MenuItem > normal </MenuItem>
+            <MenuItem> normal </MenuItem>
+            <SubMenu title="dropdown">
+                    <MenuItem> drop1 </MenuItem>    
+            </SubMenu>
         </Menu>
     )
 }
 
+const createStyleFile = ()=>{
+    const cssFile: string = `
+        .viking-submenu {
+            display: none;
+        }
+        .viking-submenu.menu-opened {
+            display: block;
+        }
+    `;
+    const style = document.createElement('style');
+    style.type = 'text/css';
+    style.innerHTML = cssFile;
+    return style;
+}
 
 let wrapper: RenderResult, menuElement: HTMLElement, activeElement: HTMLElement, disabledElement: HTMLElement
 describe('test Menu and MenuItem component', () => {
     beforeEach(()=>{
         wrapper = render(NiceMenu(testProps))
+        wrapper.container.append(createStyleFile())
         menuElement = wrapper.getByTestId('test-menu')
         activeElement = wrapper.getByText('active')
         disabledElement = wrapper.getByText('disabled')      
@@ -34,7 +53,7 @@ describe('test Menu and MenuItem component', () => {
     it('should render correct Menu and MenuItem based on default props', () => {
        expect(menuElement).toBeInTheDocument()
        expect(menuElement).toHaveClass('viking-menu test')
-       expect(menuElement.getElementsByTagName('li').length).toEqual(3)
+       expect(menuElement.querySelectorAll(':scope > li').length).toEqual(4)
        expect(activeElement).toHaveClass('menu-item is-active')
        expect(disabledElement).toHaveClass('menu-item is-disabled')
     })
@@ -43,15 +62,32 @@ describe('test Menu and MenuItem component', () => {
         fireEvent.click(thirdItem)
         expect(thirdItem).toHaveClass('is-active')
         expect(activeElement).not.toHaveClass('is-active')
-        expect(testProps.onSelect).toHaveBeenCalledWith(2)
+        expect(testProps.onSelect).toHaveBeenCalledWith('2')
         fireEvent.click(disabledElement)
         expect(disabledElement).not.toHaveClass('is-active')
-        expect(testProps.onSelect).not.toHaveBeenCalledWith(1)
+        expect(testProps.onSelect).not.toHaveBeenCalledWith('1')
     })
     it('should render vertical mode when mode is set to vertical', () => {
         cleanup()
         const wrapper = render(NiceMenu(testVerProps))
         const menuElement = wrapper.getByTestId('test-menu')
         expect(menuElement).toHaveClass('menu-vertical')
+    })
+    it('should show dropdown items when hover on subMenu', async ()=>{
+        expect(wrapper.queryByText('drop1')).not.toBeVisible()
+        const dropdownElement = wrapper.getByText('dropdown')
+        fireEvent.mouseEnter(dropdownElement)
+        await waitFor(() => {
+            expect(wrapper.queryByText('dropdown')).toBeVisible(); // 显示出来
+        });
+
+        // 显示出来后，点击下拉菜单第一个item
+        fireEvent.click(wrapper.getByText('drop1')); // 点击这个item， 触发 onSelect 事件
+        expect(testProps.onSelect).toHaveBeenCalledWith('3-0'); // 返回参数 是不是  3-0 
+        // 鼠标离开
+        fireEvent.mouseLeave(dropdownElement); 
+        await waitFor(() => {
+            expect(wrapper.queryByText('drop1')).not.toBeVisible(); // 消失
+        });
     })
 })
